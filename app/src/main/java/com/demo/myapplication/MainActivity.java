b4081d1304188b486abce086c03492028ba4fe6f
chirag1210 <chirag.chaudhari7@gmail.com>
2018-01-04 23:56:55 +0530

    sample1

---
+++ app/src/main/java/com/demo/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.demo.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+  private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(getApplicationContext(),
+                getString(R.string.adunitid));
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                // Check the LogCat to get your test device ID
+                .addTestDevice("Your App Device")
+                .build();
+        mAdView.loadAd(adRequest);
+    }
+}
